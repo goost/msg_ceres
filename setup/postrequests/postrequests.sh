6f956015749a71d871478107d5bc949f5c01fb6a
MaxAtoms <lisamith@outlook.de>
2019-11-09 12:56:28 +0100

    Added URL as command line parameter

--- setup/postrequests/postrequests.sh
+++ setup/postrequests/postrequests.sh
@@ -4,5 +4,5 @@
 for filename in users/*.json; do
     curl -X POST --header "Content-Type: application/json" \
         --data @$filename \
-        http://msg-lunchserver8.westeurope.azurecontainer.io:8080/users
+        "$1"
 done
