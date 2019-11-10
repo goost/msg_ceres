68cab70a071c1de2d6c825b5b7130c35348bd95f
MaxAtoms <lisamith@outlook.de>
2019-11-10 02:02:54 +0100

    postrequests.sh now adds a group
    The group has lunch locations, members and an election

--- setup/postrequests/postrequests.sh
+++ setup/postrequests/postrequests.sh
@@ -4,5 +4,27 @@
 for filename in users/*.json; do
     curl -X POST --header "Content-Type: application/json" \
         --data @$filename \
-        "$1"
+        "$1/users"
 done
+
+# Add group
+curl -X POST --header "Content-Type: application/json" \
+        --data @group.json \
+        "$1/groups"
+
+# Add users to group
+curl -X POST "$1/groups/1/members/1" -H  "accept: */*"
+curl -X POST "$1/groups/1/members/2" -H  "accept: */*"
+curl -X POST "$1/groups/1/members/3" -H  "accept: */*"
+
+# Add lunch locations
+for filename in lunchLocations/*.json; do
+    curl -X POST --header "Content-Type: application/json" \
+        --data @$filename \
+        "$1/groups/1/lunchLocations"
+done
+
+# Add election
+curl -X POST --header "Content-Type: application/json" \
+        --data @election.json \
+        "$1/groups/1/elections"
